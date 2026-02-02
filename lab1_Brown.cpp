aacb9b711c69fa8c42a528e2e7d3815e15cc9d17
TCBonline <trcb383@gmail.com>
2026-02-02 04:19:56 +0000

    lab1 homework

--- lab1_Brown.cpp
+++ lab1_Brown.cpp
@@ -41,5 +41,29 @@ int main(){
 
     // print results
     cout<<"Welcome to C++ "<<lastname<<" Your rate is "<<n<<endl;
+
+    cout<<"\nLab assignment 1: introduction to iostream and numerical variables"<<endl;
+    // Declare 4 variables
+    string country;
+    char gender;
+    int zipcode;
+    float rate;
+
+    cout<<"Type a country = "<<endl;
+    cin>>country;
+    cout<<"Type a gender (f for female, m for male, and o for other) = "<<endl;
+    cin>>gender;
+    cout<<"Type a zip code = "<<endl;
+    cin>>zipcode;
+    cout<<"Type a tax rate = "<<endl;
+    cin>>rate;
+
+    // print results
+    cout<<"Entered Country: "<<"\t"<<country<<endl;
+    cout<<"Selected Gender: "<<"\t"<<gender<<endl;
+    cout<<"Zip Code: "<<"\t"<<zipcode<<endl;
+    cout<<"Tax Rate: "<<"\t"<<rate<<endl;
+
     return 0;
 }
+
